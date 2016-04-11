8e7bbd91fd1b2a56a74eb902120ab1f9c1496817
shisy13 <shisy13@icloud.com>
2016-04-11 19:17:40 +0800

    __wsd done

--- votube/views.py
+++ votube/views.py
@@ -19,15 +19,18 @@ zero_time = datetime.strptime('00:00:00.000', '%H:%M:%S.%f')
 
 
 from time import clock
+
+
 def timeit(func):
     def __decorator(*args, **kwags):
         start = clock()
-        result = func(*args, **kwags)  #recevie the native function call result
+        # recevie the native function call result
+        result = func(*args, **kwags)
         finish = clock()
         span = int((finish - start) * 1000)
         if span > 500:
             print '[DEBUG] timeit: ', func.__name__, span, 'ms'
-        return result        #return to caller
+        return result  # return to caller
     return __decorator
 
 
@@ -58,9 +61,7 @@ class PageView(TemplateView):
     @timeit
     def __wsd(word_object, context):
         try:
-            word = word_object['word']
-            wordjson = dictionary.find_one({'_id': word})
-            senses = wordjson['def']
+            senses = word_object['def']
             classifiedSenses = classifySenses(senses)
             dic = sentprocesser.processDic(classifiedSenses, word)
             sent = sentprocesser.processSent(context, word)
@@ -85,12 +86,14 @@ class PageView(TemplateView):
         usphone = w.get('usphone', w.get('ukphone', ''))
         usspeech = w.get('usspeech', w.get('ukspeech', ''))
         try:
-            forms = ', '.join([o['word'] for o in word['def']['collins']['collins_entries'][0]['basic_entries']['basic_entry'][0]['wordforms']['wordform']])
+            forms = ', '.join([o['word'] for o in word['def']['collins']['collins_entries'][
+                              0]['basic_entries']['basic_entry'][0]['wordforms']['wordform']])
         except Exception, e:
             print repr(e)
             forms = ''
         try:
-            meanings = list(chain(*[[te['tran_entry'][0] for te in e['entries']['entry']] for e in word['def']['collins']['collins_entries']]))
+            meanings = list(chain(*[[te['tran_entry'][0] for te in e['entries']['entry']]
+                                    for e in word['def']['collins']['collins_entries']]))
             meanings = [m for m in meanings if m.get('tran')]
             for i, m in enumerate(meanings):
                 m['id'] = 'sense%d' % (i + 1)
@@ -119,17 +122,20 @@ class PageView(TemplateView):
     @staticmethod
     @timeit
     def __get_clips(word):
-        word['sents'] = [s for s in word['sents'] if s['movie']]    # pertain clips with movie downloads
+        # pertain clips with movie downloads
+        word['sents'] = [s for s in word['sents'] if s['movie']]
         for s in word['sents']:
             s['id'] = s['_id']
-            times = [(datetime.strptime(t, '%H:%M:%S.%f') - zero_time).total_seconds() for t in s['time']]
+            times = [(datetime.strptime(t, '%H:%M:%S.%f') -
+                      zero_time).total_seconds() for t in s['time']]
             s['time'] = times
             s['start'] = max((times[0] + times[1]) / 2, times[1] - 3)
             s['end'] = min((times[2] + times[3]) / 2, times[2] + 1)
             # s['start'] = start.strftime('%H:%M:%S.%f')[:-3]
             # s['end'] = end.strftime('%H:%M:%S.%f')[:-3]
             s['length'] = s['end'] - s['start']
-            s['morph'] = s['line'][s['line'].find('<em>')+4:s['line'].find('</em>')]
+            s['morph'] = s['line'][s['line'].find(
+                '<em>') + 4:s['line'].find('</em>')]
         return word['sents']
 
     @staticmethod
@@ -140,24 +146,31 @@ class PageView(TemplateView):
         for s in word['sents']:
             s['movie'] = d.get(s['movie'], {})
             if SNAPSHOT_FROM_CACHE and s['movie'].get('videofile'):
-                s['snapshot'] = '%s_%d.png' % (s['movie']['videofile'], int(s['time'][1]))
+                s['snapshot'] = '%s_%d.png' % (
+                    s['movie']['videofile'], int(s['time'][1]))
         for m in d.itervalues():
             assert m['videofile']
-            m['id'] = ''.join([c for c in m['_id'] if c.islower() or c.isdigit()]) # convert to safe css class name
+            # convert to safe css class name
+            m['id'] = ''.join(
+                [c for c in m['_id'] if c.islower() or c.isdigit()])
             m['rating'] = float(m['rating'])
             m['omdb']['imdbRating'] = float(m['omdb']['imdbRating'])
-            m['omdb']['imdbVotes'] = int(m['omdb']['imdbVotes'].replace(',', ''))
+            m['omdb']['imdbVotes'] = int(
+                m['omdb']['imdbVotes'].replace(',', ''))
             if 'poster' in m:
-                m['poster_cached'] = 'http://pi.cs.tsinghua.edu.cn/lab/moviedict/movies/poster/' + m['poster'].split('/')[-1]
+                m['poster_cached'] = 'http://pi.cs.tsinghua.edu.cn/lab/moviedict/movies/poster/' + \
+                    m['poster'].split('/')[-1]
             if 'Poster' in m['omdb']:
-                m['poster_cached2'] = 'http://pi.cs.tsinghua.edu.cn/lab/moviedict/movies/poster/' + m['omdb']['Poster'].split('/')[-1]
+                m['poster_cached2'] = 'http://pi.cs.tsinghua.edu.cn/lab/moviedict/movies/poster/' + \
+                    m['omdb']['Poster'].split('/')[-1]
         return d.values()
 
     @staticmethod
     def __line_length(line):
         ss = line.split()
         times = [ss[0], ss[2]]
-        times = [(datetime.strptime(t, '%H:%M:%S.%f') - zero_time).total_seconds() for t in times]
+        times = [(datetime.strptime(t, '%H:%M:%S.%f') -
+                  zero_time).total_seconds() for t in times]
         return times[1] - times[0]
 
     @classmethod
@@ -167,11 +180,11 @@ class PageView(TemplateView):
         st, num = 0, len(clips)
         toremove = []
         while st < num:
-            sentA = clips[st]['line'][clips[st]['line'].find('\n')+1:]
+            sentA = clips[st]['line'][clips[st]['line'].find('\n') + 1:]
             for i in range(st):
                 if clips[st]['movie']['videofile'] != clips[i]['movie']['videofile']:
                     continue
-                sentB = clips[i]['line'][clips[i]['line'].find('\n')+1:]
+                sentB = clips[i]['line'][clips[i]['line'].find('\n') + 1:]
                 cover = lcs(sentA, sentB)
                 ratio = 1.0 * cover / max([len(sentA), len(sentB)])
                 if ratio > 0.9:
@@ -180,8 +193,10 @@ class PageView(TemplateView):
                         tmp = clips[i]
                     toremove.append(tmp)
                 else:
-                    sentA = clips[st]['sent'].replace('<em>', '').replace('</em>', '')
-                    sentB = clips[i]['sent'].replace('<em>', '').replace('</em>', '')
+                    sentA = clips[st]['sent'].replace(
+                        '<em>', '').replace('</em>', '')
+                    sentB = clips[i]['sent'].replace(
+                        '<em>', '').replace('</em>', '')
                     if sentA.strip() == sentB.strip():
                         toremove.append(clips[st])
             st += 1
@@ -203,8 +218,10 @@ class PageView(TemplateView):
         context['word'] = self.__get_word(r)
         context['clips'] = self.__get_clips(r)
         context['movies'] = self.__get_movies(r)
-        context['clips'] = [c for c in context['clips'] if c['movie'].get('videofile')]    # pertain clips with movie file
-        context['clips'] = [c for c in context['clips'] if c['length'] < 30]    # pertain short clips
+        context['clips'] = [c for c in context['clips'] if c[
+            'movie'].get('videofile')]    # pertain clips with movie file
+        context['clips'] = [c for c in context['clips']
+                            if c['length'] < 30]    # pertain short clips
         context['clips'] = self.__distinct_clips(context['clips'])
 
         # sort context['clips']:
@@ -213,7 +230,8 @@ class PageView(TemplateView):
         # 3. by length of the occurrence line
         # 4. by reversed clip length
         # 5. by views
-        context['clips'].sort(key=lambda c: (-c.get('sense', 0), c.get('votes', 0), int(self.__line_length(c['line']) / 2), -int(c['length'] / 2), c.get('views', 0)), reverse=True)
+        context['clips'].sort(key=lambda c: (-c.get('sense', 0), c.get('votes', 0), int(
+            self.__line_length(c['line']) / 2), -int(c['length'] / 2), c.get('views', 0)), reverse=True)
         context['movies'].sort(key=lambda m: -(m.get('t', 0) + m.get('g', 0)))
 
         if context['clips']:
@@ -221,13 +239,16 @@ class PageView(TemplateView):
             ids = [c['id'] for c in context['clips']]
             index = ids.index(clip_id) if clip_id in ids else 0
             context['active_clip'] = context['clips'][index]
-            context['next_clip'] = context['clips'][(index + 1) % len(context['clips'])]
+            context['next_clip'] = context['clips'][
+                (index + 1) % len(context['clips'])]
 
         if context['movies']:
             movie_id = self.request.GET.get('movie_id', '')
             if movie_id:
-                context['clips'] = [c for c in context['clips'] if c['movie']['id'] == movie_id]
-            context['movies'] = [{'id': '', 'title': 'All Movies'}] + context['movies']
+                context['clips'] = [c for c in context[
+                    'clips'] if c['movie']['id'] == movie_id]
+            context['movies'] = [
+                {'id': '', 'title': 'All Movies'}] + context['movies']
             ids = [m['id'] for m in context['movies']]
             context['active_movie'] = context['movies'][ids.index(movie_id)]
 
@@ -235,10 +256,13 @@ class PageView(TemplateView):
             sense_id = self.request.GET.get('sense_id', '')
             word_context = self.request.GET.get('context')
             if word_context:
-                sense_id = 'sense%d' % (self.__wsd(context['raw_word'], word_context) + 1)
+                sense_id = 'sense%d' % (self.__wsd(
+                    context['word']['raw_word'], word_context) + 1)
             if sense_id:
-                context['clips'] = [c for c in context['clips'] if c.get('sense', 0) == int(sense_id[5:]) - 1]
-            meanings = [{'id': '', 'tran': 'All Meanings'}] + context['word']['meanings']
+                context['clips'] = [c for c in context['clips']
+                                    if c.get('sense', 0) == int(sense_id[5:]) - 1]
+            meanings = [{'id': '', 'tran': 'All Meanings'}] + \
+                context['word']['meanings']
             for i, m in enumerate(meanings[1:]):
                 m['index'] = '%d.' % (i + 1)
             ids = [m['id'] for m in meanings]
@@ -257,7 +281,8 @@ class PageView(TemplateView):
         sessionid = request.POST.get('sessionid', 'unknown')
         if clip_id:
             print 'vote on %s by %s' % (clip_id, sessionid)
-            clip = db.sents.find_one_and_update({'_id': clip_id}, {'$inc': {'votes': 1}}, return_document=ReturnDocument.AFTER)
+            clip = db.sents.find_one_and_update(
+                {'_id': clip_id}, {'$inc': {'votes': 1}}, return_document=ReturnDocument.AFTER)
             return HttpResponse(str(clip['votes']))
 
 
@@ -265,5 +290,6 @@ class AnalyticView(View):
 
     def get(self, request, *args, **kwargs):
         clip_id = request.GET.get('clip_id')
-        clip = db.sents.find_one_and_update({'_id': clip_id}, {'$inc': {'views': 1}}, return_document=ReturnDocument.AFTER)
+        clip = db.sents.find_one_and_update(
+            {'_id': clip_id}, {'$inc': {'views': 1}}, return_document=ReturnDocument.AFTER)
         return HttpResponse(str(clip['views']))
